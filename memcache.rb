feabd952d2d1cc2c22f6b32fd8fb7a0f743fd7d8
Andres Puglia <32549160+AndresPuglia98@users.noreply.github.com>
2021-02-11 19:48:09 -0300

    Memcache get and set

--- memcache.rb
+++ memcache.rb
@@ -1,11 +1,48 @@
-require 'concurrent'
+# require 'concurrent'
+require_relative 'reply.rb'
+require_relative 'item.rb'
+
 
 class Memcache
 
     attr_reader :data_store
 
-    def initialize
-       @data_store Concurrent::Hash.new()
+    def initialize()
+       @data_store = Hash.new()
     end
     
+    def get(key)
+        return @data_store[key]
+    end
+
+    def gets(key)
+        
+    end
+
+    def set(key, flags, exptime, bytes, data_block)
+        item = Item.new(key, flags, exptime, bytes, data_block)
+        @data_store.store(key, item)
+        return Reply::STORED
+    end
+
+    def add(key, flags, exptime, bytes, noreply, data_block)
+        
+    end
+
+    def replace(key, flags, exptime, bytes, noreply, data_block)
+        
+    end
+
+    def append(key, flags, exptime, bytes, noreply, data_block)
+        
+    end
+
+    def prepend(key, flags, exptime, bytes, noreply, data_block)
+        
+    end
+
+    def cas(key, flags, exptime, bytes, cas_unique, noreply, data_block)
+        
+    end
+
 end
