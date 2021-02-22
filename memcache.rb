b6be634b1c8d300ee5537693395a56a035055384
Andres Puglia <32549160+AndresPuglia98@users.noreply.github.com>
2021-02-22 17:38:26 -0300

    Memcache class Update

--- memcache.rb
+++ memcache.rb
@@ -1,52 +1,84 @@
-# require 'concurrent'
 require_relative 'reply.rb'
 require_relative 'item.rb'
 
 
 class Memcache
 
-    attr_reader :data_store
+    attr_reader :storage
 
     def initialize()
-       @data_store = Hash.new()
-    end
-    
-    def get(key)
-        return @data_store[key]
+       @storage = Hash.new()
     end
 
-    def gets(key)
-        
+    def purge_keys()
+        @storage.delete_if {| key, item | item.expired?}
+    end
+
+    def get(keys)
+        purge_keys()
+        items = @storage.values_at(*keys)
+        return items
     end
 
     def set(key, flags, exptime, bytes, data_block)
-        item = Item.new(key, flags, exptime, bytes, data_block)
-        @data_store.store(key, item)
+        store_item(key, flags, exptime, bytes, data_block)
         return Reply::STORED
     end
 
-    def add(key, flags, exptime, bytes, noreply, data_block)
+    def add(key, flags, exptime, bytes, data_block)
+        purge_keys()
         return Reply::NOT_STORED if @storage.key?(key)
         
-        item = Item.new(key, flags, exptime, bytes, data_block)
-        @storage.store(key, item)
+        store_item(key, flags, exptime, bytes, data_block)
         return Reply::STORED
     end
 
-    def replace(key, flags, exptime, bytes, noreply, data_block)
-        
+    def replace(key, flags, exptime, bytes, data_block)
+        return Reply::NOT_STORED unless @storage.key?(key)
+
+        store_item(key, flags, exptime, bytes, data_block)
+        return Reply::STORED
     end
 
-    def append(key, flags, exptime, bytes, noreply, data_block)
-        
+    def append(key, bytes, new_data)
+        purge_keys()
+        return Reply::NOT_STORED unless @storage.key?(key)
+
+        item = @storage[key]
+        item.data_block.concat(new_data)
+        item.bytes += bytes
+        item.update_cas_id()
+        return Reply::STORED
     end
 
-    def prepend(key, flags, exptime, bytes, noreply, data_block)
-        
+    def prepend(key, bytes, new_data)
+        purge_keys()
+        return Reply::NOT_STORED unless @storage.key?(key)
+
+        item = @storage[key]
+        item.data_block.prepend(new_data)
+        item.bytes += bytes
+        item.update_cas_id()
+        return Reply::STORED
     end
 
-    def cas(key, flags, exptime, bytes, cas_unique, noreply, data_block)
-        
+    def cas(key, flags, exptime, bytes, cas_id, data_block)
+        purge_keys()
+        return Reply::NOT_FOUND unless @storage.key?(key)
+
+        item = @storage[key]
+        item.mutex.synchronize do
+            return Reply::EXISTS if cas_id != item.cas_id
+    
+            store_item(key, flags, exptime, bytes, data_block)
+            return Reply::STORED
+        end
+    end
+
+    def store_item(key, flags, exptime, bytes, data_block)
+        item = Item.new(key, flags, exptime, bytes, data_block)
+        item.update_cas_id()
+        @storage.store(key, item) unless item.expired?
     end
 
 end
