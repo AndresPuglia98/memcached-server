5544be97f3501ec99fbb6886a0168bc67fc4b836
Andres Puglia <32549160+AndresPuglia98@users.noreply.github.com>
2021-02-22 17:37:08 -0300

    Constants

--- reply.rb
+++ reply.rb
@@ -1,6 +1,13 @@
 class Reply
+
     STORED = "STORED\n".freeze
     NOT_STORED = "NOT_STORED\n".freeze
     EXISTS = "EXISTS\n".freeze
     NOT_FOUND = "NOT_FOUND\n".freeze
+
+    GET = "VALUE %s %d %d\n%s\n".freeze
+    GETS = "VALUE %s %d %d %d\n%s\n".freeze
+
+    END_ = "END\n".freeze
+
 end
