b9b9a2f115a7fa71af130bf1dd33340c10d0b624
Andres Puglia <32549160+AndresPuglia98@users.noreply.github.com>
2021-02-22 17:37:20 -0300

    Item class

--- item.rb
+++ item.rb
@@ -4,19 +4,42 @@ class Item
     attr_accessor :flags
     attr_accessor :exptime
     attr_accessor :bytes
-    attr_accessor :cas_unique
     attr_accessor :data_block
+    attr_accessor :cas_id
+    attr_accessor :mutex
+
+    @@last_cas_id = 0
 
     def initialize(key, flags, exptime, bytes, data_block)
         @key = key
         @flags = flags
         @bytes = bytes
-        @exptime = exptime
+        @exptime = get_exptime(exptime)
         @data_block = data_block
+        @cas_id = cas_id
+        @mutex = Mutex.new()
     end
 
-    def to_s()
-        return "Item key: #{@key}"
+    def get_cas_id()
+        @mutex.synchronize do
+            @@last_cas_id += 1
+            next_id = @@last_cas_id.dup()
+            return next_id
+        end
     end
 
+    def update_cas_id()
+        @cas_id = get_cas_id()
+    end
+
+    def get_exptime(exptime)
+        return nil if exptime == 0
+        return Time.now().getutc() if exptime < 0
+        return Time.now().getutc() + exptime
+    end
+
+    def expired?()
+        return true if (!@exptime.nil?()) && (Time.now().getutc() > @exptime)
+        return false
+    end
 end
